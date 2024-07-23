6f418192e447beb669ef802a05689a077672b68e
Shivam <shivamk200227@gmail.com>
2024-07-23 21:36:02 +0530

    made utils for success and errors from APIs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: './env' });
 
 
-connectDB()
+
+connectDB().then(() => {
+    app.on('error', (error) => {
+        console.error("Express error", error)
+        throw new Error(error);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+}).catch((error) => {
+    console.error("Database connection error", error);
+    process.exit(1);
+});
